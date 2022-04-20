7ead5a7a75597f0dd6ffed29bda832db2dbc3088
kevingzhang <kevin.zhang.canada@gmail.com>
2022-04-19 17:17:20 -0700

    support cors, support static file load from port8080

--- backend/services/src/main.rs
+++ backend/services/src/main.rs
@@ -2,25 +2,76 @@ mod tokendb;
 mod error;
 #[macro_use]
 extern crate log;
-/// Use axum capabities.
-use axum::routing::{post};
+use tower_http::cors::{CorsLayer, Origin};
+use axum::{
+    http::{header, Method, StatusCode},
+    response::{Html, IntoResponse},
+    routing::{get,post, get_service},
+    Json, Router,
+};
+use std::net::SocketAddr;
 use serde_json::{json, Value};
-
+use tower_http::{services::ServeDir, trace::TraceLayer};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
-async fn main() {
-    // Build our application by creating our router.
-    let app = axum::Router::new()
-        .route("/api/get_article_list_and_view",
-            // get(get_books)
-            post(article_list_and_view)
-        )
-        .route("/api/check_already_paid",
-            post(check_already_paid)
+async fn main(){     // create our static file handler
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::new(
+            std::env::var("RUST_LOG")
+                .unwrap_or_else(|_| "example_static_file_server=debug,tower_http=debug".into()),
+        ))
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+    let frontend = async {
+        let app = Router::new()
+            .nest(
+                "/",
+                get_service(ServeDir::new("../../frontend/website/"))
+                .handle_error(|error: std::io::Error| async move {
+                    (
+                        StatusCode::INTERNAL_SERVER_ERROR,
+                        format!("Unhandled internal error: {}", error),
+                    )
+                }),
+            )
+            .layer(TraceLayer::new_for_http());
+        serve(app, 8080).await;
+    };
+    let backend = async {
+        let app = Router::new()
+            .route("/api/get_article_list_and_view", post(article_list_and_view))
+            .route("/api/check_already_paid", post(check_already_paid))
+            .layer(
+            // see https://docs.rs/tower-http/latest/tower_http/cors/index.html
+            // for more details
+            //
+            // pay attention that for some request types like posting content-type: application/json
+            // it is required to add ".allow_headers(vec![http::header::CONTENT_TYPE])"
+            // or see this issue https://github.com/tokio-rs/axum/issues/849
+            CorsLayer::new()
+                // .allow_origins(AllowedOrigins::Any { allow_null: false })
+                .allow_headers([
+                    header::ACCEPT,
+                    header::CONTENT_TYPE,
+                    header::CONTENT_LENGTH,
+                    header::ACCEPT_ENCODING,
+                    header::ACCEPT_LANGUAGE,
+                    header::AUTHORIZATION,
+                ])
+                .allow_origin(Origin::exact("http://localhost:3000".parse().unwrap()))
+                .allow_methods(vec![Method::GET, Method::POST]),
         );
+        serve(app, 3000).await;
+    };
 
-    println!("Run our application as a hyper server on http://localhost:3000.");
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    tokio::join!(frontend, backend);
+
+}
+async fn serve(app: Router, port: u16) {
+    let addr = SocketAddr::from(([127, 0, 0, 1], port));
+    println!("Run our application as a hyper server on http://localhost:{}.", port);
+    axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
