1916a977ae39b816d66468ea4cb959d22afb5f7e
Ehtisham <eht.elahi@gmail.com>
2022-06-16 14:07:46 +0500

    fix insert query

--- backend/services/src/main.rs
+++ backend/services/src/main.rs
@@ -2,6 +2,9 @@ mod tokendb;
 mod error;
 #[macro_use]
 extern crate log;
+extern crate chrono;
+use chrono::prelude::*;
+
 use tower_http::cors::{CorsLayer, Origin};
 use axum::{
     http::{header, Method, StatusCode},
@@ -108,6 +111,13 @@ async fn main(){     // create our static file handler
             .route("/api/get_article_homepage", post(article_homepage))
             .route("/api/check_already_paid", post(check_already_paid))
             .route("/api/get_user_profile", post(get_user_profile))
+            .route("/api/save_article", post(save_article))
+            .route("/api/get_recommendations", post(get_recommendations))
+            .route("/api/search_articles", post(search_articles))
+            .route("/api/search_authors", post(search_authors))
+            .route("/api/follow", post(follow))
+            .route("/api/get_user_profile_articles", post(get_user_profile_articles))
+            .route("/api/get_notifications", post(get_notifications))
             .route("/api/sql_test", post(sql_test))
             .route("/api/sql_query", post(sql_query))
             .layer(
@@ -127,7 +137,7 @@ async fn main(){     // create our static file handler
                     header::ACCEPT_LANGUAGE,
                     header::AUTHORIZATION,
                 ])
-                .allow_origin(Origin::exact("http://localhost:8080".parse().unwrap()))
+                .allow_origin(Origin::exact("http://localhost:8888".parse().unwrap()))
                 .allow_methods(vec![Method::GET, Method::POST]),
             )
             .layer(Extension(Arc::new(AppState::new(&glue_path))));
@@ -262,25 +272,23 @@ pub async fn sql_query(
 
 fn article_list_and_view_inner(input: &serde_json::Value, state: Arc<AppState>)
         ->Result<Payload>{
-    let id_start = input.get("start_index")
-        .ok_or(anyhow!("input has no start_index"))?
+    let offset = input.get("offset")
+        .ok_or(anyhow!("input has no offset"))?
         .as_i64()
-        .ok_or(anyhow!("start_index is not a number"))?;
-    let id_end = input.get("number_of_article")
-        .ok_or(anyhow!("input has no number_of_article"))?
+        .ok_or(anyhow!("offset is not a number"))?;
+    let limit = input.get("limit")
+        .ok_or(anyhow!("input has no limit"))?
         .as_i64()
-        .ok_or(anyhow!("number_of_article is not a number"))?
-        + id_start;
-    let _user_id = input.get("user_id")
-        .ok_or(anyhow!("input has no user_id"))?
-        .as_i64()
-        .ok_or(anyhow!("user_id is not a number"))?;//we have not used it yet
+        .ok_or(anyhow!("limit is not a number"))?;
+    // let _user_id = input.get("user_id")
+    //     .ok_or(anyhow!("input has no user_id"))?
+    //     .as_i64()
+    //     .ok_or(anyhow!("user_id is not a number"))?;//we have not used it yet
 
     let sql=  format!(r#"SELECT a.article_id, a.article_title,
         b.name, b.pfp, b.total_invested, a.image_url, a.hashtag, a.date_posted
         FROM articles a INNER JOIN users b
-        ON b.id = a.author_id WHERE a.article_id >= {} AND
-        a.article_id < {}"#, id_start, id_end);
+        ON b.id = a.author_id WHERE is_published = {} ORDER BY a.date_posted DESC LIMIT {} OFFSET {}"#, true, limit, offset);
 
     let mut glue = state.glue.write().unwrap();//tokendb::init_glue(&state.glue_path).unwrap();
     exec_query(&mut glue, &sql).map_err(|e| anyhow!("GlueSQL error: {}", e.to_string()))
@@ -305,7 +313,7 @@ fn article_homepage_inner(input: &serde_json::Value, state: Arc<AppState>)
         a.price as article_price, a.liquidation_days as article_liquidation_time,
         a.article_total_reads, a.article_total_shares, a.article_description
         FROM articles a INNER JOIN users b
-        ON b.id = a.author_id WHERE a.article_id = {} "#, article_id);
+        ON b.id = a.author_id WHERE a.article_id = {} AND is_published = {}"#, article_id, true);
 
     let mut glue = state.glue.write().unwrap();//tokendb::init_glue(&state.glue_path).unwrap();
     exec_query(&mut glue, &sql).map_err(|e| anyhow!("GlueSQL error: {}", e.to_string()))
@@ -346,6 +354,28 @@ pub async fn check_already_paid(
 ) -> axum::extract::Json<Value> {
     run_sql_json(state, input, check_already_paid_inner).await
 }
+// fn get_user_profile_inner(input: &serde_json::Value, state: Arc<AppState>)
+//         ->Result<Payload>{
+//     let user_id = input.get("user_id")
+//         .ok_or(anyhow!("input has no user_id"))?
+//         .as_i64()
+//         .ok_or(anyhow!("user_id is not a number"))?;
+
+//     let sql=  format!(r#"
+//         SELECT balance as user_wallet_balance
+//         FROM users
+//         WHERE id = {} "#, user_id);
+
+//     let mut glue = state.glue.write().unwrap();//tokendb::init_glue(&state.glue_path).unwrap();
+//     exec_query(&mut glue, &sql).map_err(|e| anyhow!("GlueSQL error: {}", e.to_string()))
+// }
+// pub async fn get_user_profile(
+//     Extension(state):Extension<Arc<AppState>>,
+//     axum::extract::Json(input): axum::extract::Json<serde_json::Value>
+// ) -> axum::extract::Json<Value> {
+//     run_sql_json(state, input, get_user_profile_inner).await
+// }
+
 fn get_user_profile_inner(input: &serde_json::Value, state: Arc<AppState>)
         ->Result<Payload>{
     let user_id = input.get("user_id")
@@ -354,9 +384,11 @@ fn get_user_profile_inner(input: &serde_json::Value, state: Arc<AppState>)
         .ok_or(anyhow!("user_id is not a number"))?;
 
     let sql=  format!(r#"
-        SELECT balance as user_wallet_balance
+        SELECT name, bio, pfp, balance as user_wallet_balance,
+        (SELECT COUNT(*) FROM follows WHERE users.id = follower_id AND followed_id = {}) as followers,
+        (SELECT COUNT(*) FROM follows WHERE users.id = followed_id AND follower_id = {}) as followings
         FROM users
-        WHERE id = {} "#, user_id);
+        WHERE id = {} "#, user_id, user_id, user_id);
 
     let mut glue = state.glue.write().unwrap();//tokendb::init_glue(&state.glue_path).unwrap();
     exec_query(&mut glue, &sql).map_err(|e| anyhow!("GlueSQL error: {}", e.to_string()))
@@ -367,6 +399,30 @@ pub async fn get_user_profile(
 ) -> axum::extract::Json<Value> {
     run_sql_json(state, input, get_user_profile_inner).await
 }
+
+fn get_user_profile_articles_inner(input: &serde_json::Value, state: Arc<AppState>)
+        ->Result<Payload>{
+    let user_id = input.get("user_id")
+        .ok_or(anyhow!("input has no user_id"))?
+        .as_i64()
+        .ok_or(anyhow!("user_id is not a number"))?;//we have not used it yet
+
+    let sql=  format!(r#"SELECT a.article_id, a.article_title,
+        b.name, b.pfp, a.image_url, a.date_posted
+        FROM articles a INNER JOIN users b
+        ON b.id = a.author_id WHERE a.author_id = {} AND is_published = {}"#, user_id, true);
+
+    let mut glue = state.glue.write().unwrap();//tokendb::init_glue(&state.glue_path).unwrap();
+    exec_query(&mut glue, &sql).map_err(|e| anyhow!("GlueSQL error: {}", e.to_string()))
+}
+
+pub async fn get_user_profile_articles(
+    Extension(state):Extension<Arc<AppState>>,
+    axum::extract::Json(input): axum::extract::Json<serde_json::Value>
+) -> axum::extract::Json<Value> {
+    run_sql_json(state, input, get_user_profile_articles_inner).await
+}
+
 pub async fn run_sql_json<F>(
     state: Arc<AppState>,
     input: serde_json::Value,
@@ -387,4 +443,144 @@ where F: FnOnce(&serde_json::Value, Arc<AppState>)->Result<Payload> + Send + 'st
             })
         }
     }).join().unwrap().into()
+}
+
+fn get_recommendations_inner(input: &serde_json::Value, state: Arc<AppState>)
+        ->Result<Payload>{
+    let offset = input.get("offset")
+        .ok_or(anyhow!("input has no offset"))?
+        .as_i64()
+        .ok_or(anyhow!("offset is not a number"))?;
+    let limit = input.get("limit")
+        .ok_or(anyhow!("input has no limit"))?
+        .as_i64()
+        .ok_or(anyhow!("limit is not a number"))?;
+
+    let sql=  format!(r#"SELECT a.article_id, a.article_title,
+        b.name, b.pfp, b.total_invested, a.image_url, a.hashtag, a.date_posted
+        FROM articles a INNER JOIN users b
+        ON b.id = a.author_id WHERE is_published = {} ORDER BY a.date_posted DESC LIMIT {} OFFSET {}"#, true, limit, offset);
+
+    let mut glue = state.glue.write().unwrap();//tokendb::init_glue(&state.glue_path).unwrap();
+    exec_query(&mut glue, &sql).map_err(|e| anyhow!("GlueSQL error: {}", e.to_string()))
+}
+pub async fn get_recommendations(
+    Extension(state):Extension<Arc<AppState>>,
+    axum::extract::Json(input): axum::extract::Json<serde_json::Value>
+) -> axum::extract::Json<Value> {
+    run_sql_json(state, input, get_recommendations_inner).await
+}
+
+fn search_articles_inner(input: &serde_json::Value, state: Arc<AppState>)
+        ->Result<Payload>{
+    let q = input.get("q")
+        .ok_or(anyhow!("input has no query"))?;
+
+    let sql=  format!(r#"SELECT a.article_id, a.article_title,
+        b.name, b.pfp, b.total_invested, a.image_url, a.hashtag, a.date_posted
+        FROM articles a INNER JOIN users b
+        ON b.id = a.author_id WHERE a.article_title LIKE {} AND is_published = {}"#, q , true);
+
+    let mut glue = state.glue.write().unwrap();//tokendb::init_glue(&state.glue_path).unwrap();
+    exec_query(&mut glue, &sql).map_err(|e| anyhow!("GlueSQL error: {}", e.to_string()))
+}
+pub async fn search_articles(
+    Extension(state):Extension<Arc<AppState>>,
+    axum::extract::Json(input): axum::extract::Json<serde_json::Value>
+) -> axum::extract::Json<Value> {
+    run_sql_json(state, input, search_articles_inner).await
+}
+
+fn search_authors_inner(input: &serde_json::Value, state: Arc<AppState>)
+        ->Result<Payload>{
+    let q = input.get("q")
+        .ok_or(anyhow!("input has no query"))?;
+    let follower_id = input.get("follower_id")
+        .ok_or(anyhow!("input has no follower_id"))?;
+
+    let sql=  format!(r#"SELECT users.id, name, bio, pfp, (EXISTS(SELECT * FROM follows WHERE users.id = followed_id AND follower_id = {})) as is_followed FROM users WHERE name LIKE {}"#, follower_id, q);
+
+    let mut glue = state.glue.write().unwrap();//tokendb::init_glue(&state.glue_path).unwrap();
+    exec_query(&mut glue, &sql).map_err(|e| anyhow!("GlueSQL error: {}", e.to_string()))
+}
+pub async fn search_authors(
+    Extension(state):Extension<Arc<AppState>>,
+    axum::extract::Json(input): axum::extract::Json<serde_json::Value>
+) -> axum::extract::Json<Value> {
+    run_sql_json(state, input, search_authors_inner).await
+}
+
+fn follow_inner(input: &serde_json::Value, state: Arc<AppState>)
+        ->Result<Payload>{
+    let follower_id = input.get("follower_id")
+        .ok_or(anyhow!("input has no follower_id"))?;
+    let followed_id = input.get("followed_id")
+        .ok_or(anyhow!("input has no followed_id"))?;
+
+    let sql=  format!(r#"INSERT INTO follows(follower_id, followed_id) VALUE({},{})"#, follower_id, followed_id);
+
+    let mut glue = state.glue.write().unwrap();//tokendb::init_glue(&state.glue_path).unwrap();
+    exec_query(&mut glue, &sql).map_err(|e| anyhow!("GlueSQL error: {}", e.to_string()))
+}
+pub async fn follow(
+    Extension(state):Extension<Arc<AppState>>,
+    axum::extract::Json(input): axum::extract::Json<serde_json::Value>
+) -> axum::extract::Json<Value> {
+    run_sql_json(state, input, follow_inner).await
+}
+
+fn get_notifications_inner(input: &serde_json::Value, state: Arc<AppState>)
+        ->Result<Payload>{
+    let user_id = input.get("user_id")
+        .ok_or(anyhow!("input has no user_id"))?;
+
+    let sql=  format!(r#"SELECT text,read_at FROM user_notification INNER JOIN notifications ORDER BY created_at DESC WHERE user_id = {}"#, user_id);
+
+    let mut glue = state.glue.write().unwrap();//tokendb::init_glue(&state.glue_path).unwrap();
+    exec_query(&mut glue, &sql).map_err(|e| anyhow!("GlueSQL error: {}", e.to_string()))
+}
+pub async fn get_notifications(
+    Extension(state):Extension<Arc<AppState>>,
+    axum::extract::Json(input): axum::extract::Json<serde_json::Value>
+) -> axum::extract::Json<Value> {
+    run_sql_json(state, input, get_notifications_inner).await
+}
+
+fn save_article_inner(input: &serde_json::Value, state: Arc<AppState>)
+        ->Result<Payload>{
+    let article_title = input.get("article_title")
+        .ok_or(anyhow!("input has no article_title"))?;
+    let article_description = input.get("article_description")
+        .ok_or(anyhow!("input has no article_description"))?;
+    let user_id = input.get("user_id")
+        .ok_or(anyhow!("input has no user_id"))?;
+    let image_url = input.get("image_url")
+        .ok_or(anyhow!("input has no image_url"))?;
+
+    // Save image to some folder and store path in a variable
+
+    let sql=  format!(r#"INSERT INTO articles (article_id, author_id, article_title, article_description, image_url, is_published) VALUES (5,{},{},{},{},{})
+         "#, user_id, article_title, article_description, image_url, false);
+
+    // Run this query and get `id` of the record and store in a variable
+    let notification_text = "Some random text";
+    let sql=  format!(r#"INSERT INTO notifications (text) VALUES ({})
+         "#, notification_text);
+
+    let notification_id = "";
+    let local: DateTime<Local> = Local::now();
+
+    let sql=  format!(r#"INSERT INTO user_notification (notification_id, user_id, created_at) VALUES ({},{},{})
+         "#, notification_id, user_id, local.timestamp());
+
+
+    let mut glue = state.glue.write().unwrap();//tokendb::init_glue(&state.glue_path).unwrap();
+    exec_cmd(&mut glue, &sql).map_err(|e| anyhow!("GlueSQL error: {}", e.to_string()))
+}
+
+pub async fn save_article(
+    Extension(state):Extension<Arc<AppState>>,
+    axum::extract::Json(input): axum::extract::Json<serde_json::Value>
+) -> axum::extract::Json<Value> {
+    run_sql_json(state, input, save_article_inner).await
 }
